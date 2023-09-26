fc3ea6171ac411b022d4beb264979b493733457d
Faisal Khan <md.fk@somaiya.edu>
2023-09-27 00:24:26 +0530

    chANGE

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -114,7 +114,7 @@ export default function Signup() {
                     validate: (value, formValues) =>
                       value === formValues.password || 'password not matching',
                   })}
-                  type='confirmPassword'
+                  type='password'
                   className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
                 {errors.confirmPassword && (
